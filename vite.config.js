76fe7c9b7e2244734104336db4343ced2a55a42f
Igor Storchevoy <hellsgor@gmail.com>
2024-05-06 12:22:09 +0400

    добавлен handlebars

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import {defineConfig} from "vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+import { store } from './src/store/store.js';
+
+const pageData = {
+  '/index.html': store,
+};
 
 export default defineConfig({
   css: {
     devSourcemap: true,
   },
-})
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, './src/components'),
+      context(pagePath) {
+        return pageData[pagePath];
+      },
+    }),
+  ],
+});
